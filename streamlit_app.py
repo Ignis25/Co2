77e4088dbb5c3a66ebdeefbba382ccc1a0b30cc9
Ignis25 <146935227+Ignis25@users.noreply.github.com>
2024-06-22 23:44:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ import streamlit as st
 st.title("Projet Co2")
 st.write("Projet réalisé par François Vergne, Drazen Saric & Arnaud Colombel")
 st.sidebar.title("Sommaire")
-pages["Présentation du Projet", Data Visualisation", "Machine Learning", "Résultats"
+pages["Présentation du Projet", Data Visualisation", "Machine Learning", "Résultats")
 if st.checkbox("Afficher"):
   st.write("Suite du Streamlit")
