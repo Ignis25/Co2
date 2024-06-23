55cc19dd95361777c71936efb2c2297750f13bb5
Ignis25 <146935227+Ignis25@users.noreply.github.com>
2024-06-23 18:54:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
+donnees2013 = pd.read_csv('2013_data.csv', sep = ';', encoding = 'latin-1')
+
 st.title("Projet Co2")
 st.write("Projet réalisé par François Vergne, Drazen Saric & Arnaud Colombel")
 st.sidebar.title("Sommaire")
