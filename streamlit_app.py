d77dba3d907ce7a4d527a84f769e2301a43a18bc
Ignis25 <146935227+Ignis25@users.noreply.github.com>
2024-06-23 21:57:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,9 @@ pages=["Présentation du Projet", "Data Visualisation", "Machine Learning", "Ré
 page=st.sidebar.radio("Aller vers", pages)
 if st.checkbox("Afficher"):
   st.write("Suite du Streamlit")
+  
+if page == page[0]
+st.dataframe(donnees2013.head(10))
 
 if page == page[1]:
   st.write("### Data Visualisation")
