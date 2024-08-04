e42d193a4c98356ab0542a2487a90809d761a3d2
Ignis25 <146935227+Ignis25@users.noreply.github.com>
2024-08-04 17:17:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -615,3 +615,9 @@ if page == pages[5]:
 algo_options = ["Arbre de décision", "Arbre de régression", "Régression linéaire","Régression logistique","Grid Search CV avec Random Forest"]
 algo_selected = st.selectbox("Veuillez choisir un algorythme:",
                               options = algo_options)
+if algo_options == "Arbre de décision":
+    st.write("MAE =", round(mae, 3))
+    st.write("\n")
+    st.write("MSE =", round(mse, 3))
+    st.write("\n")
+    st.write("RMSE =", round(rmse, 3))
